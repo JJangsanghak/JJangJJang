bf6784ca5aba8b5c52c47ab7d8c6e79fa69f12b3
JJangsanghak <163820811+JJangsanghak@users.noreply.github.com>
2024-04-08 22:58:59 +0900

    Add files via upload

---
+++ hw4.py
@@ -0,0 +1,13 @@
+def rep_char(c,n):
+    print(c*n)
+
+def draw_line_string():
+    a=input("Input his/her name: ")
+    msg1="Hello"+' '+a+","
+    msg2="welcome to Seoul."
+    nstr=len(msg1)if(len(msg1)>len(msg2))else len(msg2)
+    rep_char("-",nstr)
+    print(msg1)
+    print(msg2)
+    rep_char("-",nstr)
+
